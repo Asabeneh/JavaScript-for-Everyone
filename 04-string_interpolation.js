bc057a5b91ab23a96ff151fd8794a30b25f34929
unknown <asabeneh@gmail.com>
2019-02-21 01:26:13 +0200

    example added to string literal.js

--- 04-string_interpolation.js
+++ 04-string_interpolation.js
@@ -11,6 +11,18 @@ console.log(personInfo);
 let numberOne = 10;
 let numberTwo = 90;
 console.log(`The sum of ${numberOne} and ${numberTwo} is ${numberOne + numberTwo}.`);
+//More Examples
+var gravity = 9.81;
+var boilingPoint = 100;
+var bodyTemp = 37;
+
+/*
+ The boiling point of water is 100 oC.
+ Human body temperatue is 37 oC.
+ The gravity of earth is 9.81 m/s2.
+ */
+console.log(`The boiling point of water is ${boilingPoint} oC.\nHuman body temperatue is ${body} oC.\nThe gravity of earth is ${gravity} m / s2.`
+);
 
 
 
