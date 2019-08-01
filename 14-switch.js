882a4b6be15edcd1aaec1f2adab6187ba5a30c09
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2019-08-01 13:39:59 +0100

    added a switch case example using a number as a variable

--- 14-switch.js
+++ 14-switch.js
@@ -16,32 +16,55 @@ switch (weather) {
 }
 
 // Swithc More Examples
-var dayUserInput = prompt('What day is it ?');
+var dayUserInput = prompt("What day is it ?");
 var day = dayUserInput.toLowerCase();
 switch (day) {
-  case 'monday':
-    console.log('Today is Monday');
+  case "monday":
+    console.log("Today is Monday");
     break;
-  case 'tuesday':
-    console.log('Today is Tuesday');
+  case "tuesday":
+    console.log("Today is Tuesday");
     break;
-  case 'wednesday':
-    console.log('Today is Wednesday');
+  case "wednesday":
+    console.log("Today is Wednesday");
     break;
-  case 'thursday':
-    console.log('Today is Thursday');
+  case "thursday":
+    console.log("Today is Thursday");
     break;
-  case 'friday':
-    console.log('Today is Friday');
+  case "friday":
+    console.log("Today is Friday");
     break;
-  case 'saturday':
-    console.log('Today is Saturday');
+  case "saturday":
+    console.log("Today is Saturday");
     break;
-  case 'sunday':
-    console.log('Today is Sunday');
+  case "sunday":
+    console.log("Today is Sunday");
     break;
 
   default:
-    console.log('It is not a week day.');
+    console.log("It is not a week day.");
     break;
+}
+
+//Example using numbers
+var i = 5;
+
+switch (i) {
+  case 1:
+    console.log("value is 1");
+    break;
+  case 2:
+    console.log("value is 2");
+    break;
+  case 3:
+    console.log("value is 3");
+    break;
+  case 4:
+    console.log("value is 4");
+    break;
+  case 5:
+    console.log("value is 5");
+    break;
+  default:
+    console.log("value not found");
 }
